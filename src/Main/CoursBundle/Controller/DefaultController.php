81d92686fc68908fb5542e23d1c70c0b41f0f950
130bby <mettere@hotmail.fr>
2017-05-24 21:31:03 +0200

    Mise en forme des cours

--- src/Main/CoursBundle/Controller/DefaultController.php
+++ src/Main/CoursBundle/Controller/DefaultController.php
@@ -10,6 +10,18 @@ class DefaultController extends Controller
     {
 		$em = $this->getDoctrine()->getManager();
         $cours = $em->getRepository('MainCoursBundle:Cours')->findBy(array('savoir' => $savoir_id));
-		return $this->render('MainCoursBundle:Default:show.html.twig', array('cours' => $cours));
+        $savoir = $em->getRepository('MainSavoirBundle:Savoir')->find($savoir_id);
+		$prerequis = array();
+		$enfants = array();
+		foreach ($savoir->getPrerequis() as $prerekiki)
+			$prerequis[] = $em->getRepository('MainSavoirBundle:Savoir')->find($prerekiki);
+			
+        $savoirs = $em->getRepository('MainSavoirBundle:Savoir')->findAll();
+		foreach ($savoirs as $savoir_aimer)
+		{
+			if (in_array($savoir->getId(),$savoir_aimer->getPrerequis()))
+				$enfants[] = $savoir_aimer;
+		}
+		return $this->render('MainCoursBundle:Default:show.html.twig', array('cours' => $cours,'prerequis' => $prerequis,'enfants' => $enfants));
     }
 }
